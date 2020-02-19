0f4aaf7944209aedc5c9a0246dd3fa6644b50ed2
matijatomc <55134628+matijatomc@users.noreply.github.com>
2020-02-19 22:27:17 +0100

    Add files via upload

---
+++ tomcMatijaPetlje9/tomcMatijaPetlje9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tomcMatijaPetlje9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.PI);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ tomcMatijaPolja8/tomcMatijaPolja8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tomcMatijaPolja8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int zbroj = 0;
+            Console.WriteLine("Upisujte vrijednosti karta u intervalu od 1 do 13 sve dog zbroj karta ne bude 31");
+            while (!(zbroj > 30))
+            {
+                int br = Convert.ToInt32(Console.ReadLine());
+                if (br > 0 && br < 14)
+                {
+                    zbroj = zbroj + br;
+                }
+                else
+                {
+                    Console.WriteLine("Upisao si broj koji nije u intervalu od 1-13");
+                }
+
+
+
+            }
+            Console.WriteLine("Rezultat: " + zbroj);
+            if (zbroj == 31) Console.WriteLine("Pobijedili ste");
+            if (zbroj > 31) Console.WriteLine("Izgubili ste");
+            Console.ReadKey();
+        }
+    }
+}
