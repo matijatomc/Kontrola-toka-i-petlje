a5d7b838bec4d7d82c3a8b1b7fcb833c6b7b72a0
matijatomc <55134628+matijatomc@users.noreply.github.com>
2020-02-19 22:41:08 +0100

    Add files via upload

---
+++ tomcMatijaPetlje10/tomcMatijaPetlje10/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tomcMatijaPetlje10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upisite broj i program ce napisat dali se duplaju znakovi");
+            int velicina;
+            int br = Convert.ToInt32(Console.ReadLine());
+            velicina = br.ToString().Length;
+            int[] arr = new int[velicina];
+            int counter = 0;
+            while (br != 0)
+            {
+                int temp = br;
+                br = br / 10;
+                arr[counter] = temp - (br * 10);
+                counter++;
+            }
+            bool ima = false;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (i != j)
+                    {
+                        if (arr[i] == arr[j])
+                        {
+                            ima = true;
+
+                        }
+                    }
+                }
+            }
+            if (ima == true)
+            {
+                Console.WriteLine("Ne");
+            }
+            else
+            {
+                Console.WriteLine("Da");
+            }
+            Console.ReadKey();
+        }
+    }
+}
